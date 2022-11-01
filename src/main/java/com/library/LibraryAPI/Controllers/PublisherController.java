bb61438e785b85e590444dfbad96431b47c4797a
joao.grilo <joao.grilo@meta.com.br>
2022-11-01 09:42:24 -0300

    Comunicação de Publisher setada

--- src/main/java/com/library/LibraryAPI/Controllers/PublisherController.java
+++ src/main/java/com/library/LibraryAPI/Controllers/PublisherController.java
@@ -1,4 +1,14 @@
 package com.library.LibraryAPI.Controllers;
 
+import com.library.LibraryAPI.Services.PublisherService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PublisherController {
+    final PublisherService publisherService;
+
+    public PublisherController(PublisherService publisherService) {
+        this.publisherService = publisherService;
+    }
 }
--- src/main/java/com/library/LibraryAPI/Services/PublisherService.java
+++ src/main/java/com/library/LibraryAPI/Services/PublisherService.java
@@ -1,4 +1,13 @@
 package com.library.LibraryAPI.Services;
 
+import com.library.LibraryAPI.Respositories.PublisherRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PublisherService {
+    final PublisherRepository publisherRepository;
+
+    public PublisherService(PublisherRepository publisherRepository) {
+        this.publisherRepository = publisherRepository;
+    }
 }
