f820738b0c58b60a77104bd5c960357036132ddb
joao.grilo <joao.grilo@meta.com.br>
2022-11-01 09:57:02 -0300

    Comunicação de Writer setada

--- src/main/java/com/library/LibraryAPI/Controllers/WriterController.java
+++ src/main/java/com/library/LibraryAPI/Controllers/WriterController.java
@@ -1,4 +1,11 @@
 package com.library.LibraryAPI.Controllers;
 
+import com.library.LibraryAPI.Services.WriterService;
+
 public class WriterController {
+    final WriterService writerService;
+
+    public WriterController(WriterService writerService) {
+        this.writerService = writerService;
+    }
 }
--- src/main/java/com/library/LibraryAPI/Services/WriterService.java
+++ src/main/java/com/library/LibraryAPI/Services/WriterService.java
@@ -1,4 +1,11 @@
 package com.library.LibraryAPI.Services;
 
+import com.library.LibraryAPI.Respositories.WriterRepository;
+
 public class WriterService {
+    final WriterRepository writerRepository;
+
+    public WriterService(WriterRepository writerRepository) {
+        this.writerRepository = writerRepository;
+    }
 }
