207d3feb7d12b06d98d6511dc73223e322831bb4
joao.grilo <joao.grilo@meta.com.br>
2022-11-01 09:35:13 -0300

    Comunicação de Book setada

--- src/main/java/com/library/LibraryAPI/Controllers/BookController.java
+++ src/main/java/com/library/LibraryAPI/Controllers/BookController.java
@@ -15,5 +15,4 @@ public class BookController {
         this.bookService = bookService;
     }
 
-
 }
--- src/main/java/com/library/LibraryAPI/Services/BookService.java
+++ src/main/java/com/library/LibraryAPI/Services/BookService.java
@@ -1,4 +1,20 @@
 package com.library.LibraryAPI.Services;
 
+import com.library.LibraryAPI.Respositories.BookRepository;
+import com.library.LibraryAPI.Respositories.PublisherRepository;
+import com.library.LibraryAPI.Respositories.WriterRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookService {
+    final PublisherRepository publisherRepository;
+    final WriterRepository writerRepository;
+    final BookRepository bookRepository;
+
+    public BookService(PublisherRepository publisherRepository, WriterRepository writerRepository, BookRepository bookRepository) {
+        this.publisherRepository = publisherRepository;
+        this.writerRepository = writerRepository;
+        this.bookRepository = bookRepository;
+    }
+
 }
